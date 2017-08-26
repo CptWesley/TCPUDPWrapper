d6e63041ac195e9e8e91eb3b5c17c3c956823ee7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-26 09:28:07 +0200

    Add DisconnectedByServerException class

---
+++ src/DisconnectedByServerException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TCPUDPWrapper
+{
+    public class DisconnectedByServerException : Exception
+    {
+        public DisconnectedByServerException()
+        {
+        }
+
+        public DisconnectedByServerException(string message) : base(message)
+        {
+        }
+
+        public DisconnectedByServerException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
