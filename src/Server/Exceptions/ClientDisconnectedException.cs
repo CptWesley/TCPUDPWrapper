e8475fae386132e158848bc61ee954a43eccca89
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-27 15:09:10 +0200

    Add exception class for client disconnects

---
+++ src/Server/Exceptions/ClientDisconnectedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TCPUDPWrapper.Server.Exceptions
+{
+    public class ClientDisconnectedException : Exception
+    {
+        public ClientDisconnectedException()
+        {
+        }
+
+        public ClientDisconnectedException(string message) : base(message)
+        {
+        }
+
+        public ClientDisconnectedException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
