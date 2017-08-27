6ded53d4fd75370c24e828c8f35c86deaf7b6d2c
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-27 14:25:32 +0200

    Add ClientConnection class

---
+++ src/Server/ClientConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPUDPWrapper.Server
+{
+    public class ClientConnection
+    {
+        public int Id { get; }
+        public TcpClient TcpClient { get; }
+        public IPAddress Ip => ((IPEndPoint)TcpClient.Client.RemoteEndPoint).Address;
+        public long Joined { get; }
+
+        // Constructor for client connection info.
+        public ClientConnection(TcpClient client, int id)
+        {
+            Id = id;
+            TcpClient = client;
+            Joined = (long) (DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
+    }
+}
