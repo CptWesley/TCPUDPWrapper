ef18381b8971849681f26e2f90e15baa02abe431
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-27 13:45:52 +0200

    Add client side event argument classes

---
+++ src/events/ClientConnectionEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace TCPUDPWrapper.events
+{
+    public class ClientConnectionEventArgs : EventArgs
+    {
+        public Object Sender { get; }
+        public IPEndPoint EndPoint { get; }
+
+        // Event arguments for client connections.
+        public ClientConnectionEventArgs(Object sender, IPEndPoint endPoint)
+        {
+            Sender = sender;
+            EndPoint = endPoint;
+        }
+    }
+}
---
+++ src/events/ClientMessageEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TCPUDPWrapper.events
+{
+    public class ClientMessageEventArgs : EventArgs
+    {
+        public Object Sender { get; }
+        public Message Message { get; }
+
+        // Event arguments for messages received by clients.
+        public ClientMessageEventArgs(Object sender, Message message)
+        {
+            Sender = sender;
+            Message = message;
+        }
+    }
+}
