e062f476cf4dc46e172aec59d988a78397b951d5
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-27 15:09:01 +0200

    Add server event arg classes

---
+++ src/Server/Events/ServerConnectionEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TCPUDPWrapper.Server.Events
+{
+    public class ServerConnectionEventArgs : EventArgs
+    {
+        public ClientConnection ClientConnection { get; }
+
+        // Constructor for the connection events arguments.
+        public ServerConnectionEventArgs(ClientConnection clientConnection)
+        {
+            ClientConnection = clientConnection;
+        }
+    }
+}
---
+++ src/Server/Events/ServerMessageEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TCPUDPWrapper.Server.Events
+{
+    public class ServerMessageEventArgs : EventArgs
+    {
+        public ClientConnection Sender { get; }
+        public Message Message { get; }
+        public long Time { get; }
+
+        // Constructor for the server chat messages event info holder.
+        public ServerMessageEventArgs(ClientConnection sender, Message message)
+        {
+            Sender = sender;
+            Message = message;
+            Time = (long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
+    }
+}
