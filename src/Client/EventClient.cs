322e2e3aced3bb86e8f114eea6092c6967b7dce1
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-08-28 13:50:04 +0200

    Add abstract class for clients

---
+++ src/Client/EventClient.cs
@@ -0,0 +1,95 @@
+﻿using System.Net;
+using TCPUDPWrapper.Client.Events;
+
+namespace TCPUDPWrapper.Client
+{
+    /// <summary>
+    /// EventHandler for an event that is fired when a client connects to a server.
+    /// </summary>
+    /// <param name="sender">Sender of the event.</param>
+    /// <param name="e">Event arguments.</param>
+    public delegate void ClientConnectedEventHandler(object sender, ClientConnectionEventArgs e);
+    /// <summary>
+    /// EventHandler for an event that is fired when a client disconnects from a server.
+    /// </summary>
+    /// <param name="sender">Sender of the event.</param>
+    /// <param name="e">Event arguments.</param>
+    public delegate void ClientDisconnectedEventHandler(object sender, ClientConnectionEventArgs e);
+    /// <summary>
+    /// EventHandler for an event that is fired when a client receives a message from the server.
+    /// </summary>
+    /// <param name="sender">Sender of the event.</param>
+    /// <param name="e">Event arguments.</param>
+    public delegate void ClientReceivedEventHandler(object sender, ClientMessageEventArgs e);
+
+    /// <summary>
+    /// Abstract class base for event based asynchronous clients.
+    /// </summary>
+    public abstract class EventClient
+    {
+        /// <summary>
+        /// Event that is fired when a client receives a message from the server.
+        /// </summary>
+        public event ClientReceivedEventHandler Received;
+        /// <summary>
+        /// Event that is fired when a client connects to a server.
+        /// </summary>
+        public event ClientConnectedEventHandler Connected;
+        /// <summary>
+        /// Event that is fired when a client disconnects from a server.
+        /// </summary>
+        public event ClientDisconnectedEventHandler Disconnected;
+
+        /// <summary>
+        /// Used when the client receives a message.
+        /// </summary>
+        /// <param name="e">Event arguments containing the message that was received.</param>
+        protected virtual void OnReceive(ClientMessageEventArgs e)
+        {
+            Received?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Used when the client connects to a server.
+        /// </summary>
+        /// <param name="e">Event arguments containing the server info that was connected to.</param>
+        protected virtual void OnConnect(ClientConnectionEventArgs e)
+        {
+            Connected?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Used when the client disconnects from a server.
+        /// </summary>
+        /// <param name="e">Event arguments containing the server info that we disconnected from.</param>
+        protected virtual void OnDisconnect(ClientConnectionEventArgs e)
+        {
+            Disconnected?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Checks whether the client is connected.
+        /// </summary>
+        /// <returns>True if connected, false if not connected.</returns>
+        public abstract bool IsConnected();
+
+        /// <summary>
+        /// Attempts to connect to an IPEndPoint. Returns true if succesful. Returns false otherwise.
+        /// </summary>
+        /// <param name="ep">End point to connect to.</param>
+        /// <returns>Return true if connection was succesful, returns false otherwise.</returns>
+        public abstract bool Connect(IPEndPoint ep);
+
+        /// <summary>
+        /// Attempt to disconnect.
+        /// </summary>
+        /// <returns>Returns true if disconnected succesful, returns false otherwise.</returns>
+        public abstract bool Disconnect();
+
+        /// <summary>
+        /// Send data to the server.
+        /// </summary>
+        /// <param name="message">Message to send to the server.</param>
+        public abstract void Send(Message message);
+    }
+}
--- src/Client/TcpEventClient.cs
+++ src/Client/TcpEventClient.cs
@@ -8,28 +8,9 @@ using TCPUDPWrapper.Client.Events;
 namespace TCPUDPWrapper.Client
 {
     /// <summary>
-    /// EventHandler for an event that is fired when a client connects to a server.
-    /// </summary>
-    /// <param name="sender">Sender of the event.</param>
-    /// <param name="e">Event arguments.</param>
-    public delegate void ClientConnectedEventHandler(object sender, ClientConnectionEventArgs e);
-    /// <summary>
-    /// EventHandler for an event that is fired when a client disconnects from a server.
-    /// </summary>
-    /// <param name="sender">Sender of the event.</param>
-    /// <param name="e">Event arguments.</param>
-    public delegate void ClientDisconnectedEventHandler(object sender, ClientConnectionEventArgs e);
-    /// <summary>
-    /// EventHandler for an event that is fired when a client receives a message from the server.
-    /// </summary>
-    /// <param name="sender">Sender of the event.</param>
-    /// <param name="e">Event arguments.</param>
-    public delegate void ClientReceivedEventHandler(object sender, ClientMessageEventArgs e);
-
-    /// <summary>
     /// Tcp event based asynchronous client.
     /// </summary>
-    public class TcpEventClient
+    public class TcpEventClient : EventClient
     {
         /// <summary>
         /// Time to allow connection attempts to servers (in seconds).
@@ -60,19 +41,6 @@ namespace TCPUDPWrapper.Client
         private Task _readTask;
 
         /// <summary>
-        /// Event that is fired when a client receives a message from the server.
-        /// </summary>
-        public event ClientReceivedEventHandler Received;
-        /// <summary>
-        /// Event that is fired when a client connects to a server.
-        /// </summary>
-        public event ClientConnectedEventHandler Connected;
-        /// <summary>
-        /// Event that is fired when a client disconnects from a server.
-        /// </summary>
-        public event ClientDisconnectedEventHandler Disconnected;
-
-        /// <summary>
         /// Constructor for an event based tcp client.
         /// </summary>
         public TcpEventClient()
@@ -80,33 +48,6 @@ namespace TCPUDPWrapper.Client
             _receiveBufferSize = 8192;
             _sendBufferSize = 8192;
             Timeout = 3;
-        }
-
-        /// <summary>
-        /// Used when the client receives a message.
-        /// </summary>
-        /// <param name="e">Event arguments containing the message that was received.</param>
-        protected virtual void OnReceive(ClientMessageEventArgs e)
-        {
-            Received?.Invoke(this, e);
-        }
-
-        /// <summary>
-        /// Used when the client connects to a server.
-        /// </summary>
-        /// <param name="e">Event arguments containing the server info that was connected to.</param>
-        protected virtual void OnConnect(ClientConnectionEventArgs e)
-        {
-            Connected?.Invoke(this, e);
-        }
-
-        /// <summary>
-        /// Used when the client disconnects from a server.
-        /// </summary>
-        /// <param name="e">Event arguments containing the server info that we disconnected from.</param>
-        protected virtual void OnDisconnect(ClientConnectionEventArgs e)
-        {
-            Disconnected?.Invoke(this, e);
         }
 
         /// <summary>
@@ -135,7 +76,7 @@ namespace TCPUDPWrapper.Client
         /// Checks whether the client is connected.
         /// </summary>
         /// <returns>True if connected, false if not connected.</returns>
-        public bool IsConnected()
+        public override bool IsConnected()
         {
             if (_client == null)
                 return false;
@@ -147,7 +88,7 @@ namespace TCPUDPWrapper.Client
         /// </summary>
         /// <param name="ep">End point to connect to.</param>
         /// <returns>Return true if connection was succesful, returns false otherwise.</returns>
-        public bool Connect(IPEndPoint ep)
+        public override bool Connect(IPEndPoint ep)
         {
             if (IsConnected())
                 return false;
@@ -183,7 +124,7 @@ namespace TCPUDPWrapper.Client
         /// Attempt to disconnect.
         /// </summary>
         /// <returns>Returns true if disconnected succesful, returns false otherwise.</returns>
-        public bool Disconnect()
+        public override bool Disconnect()
         {
             if (_client == null)
                 return false;
@@ -243,7 +184,7 @@ namespace TCPUDPWrapper.Client
         /// Send data to the server.
         /// </summary>
         /// <param name="message">Message to send to the server.</param>
-        public void Send(Message message)
+        public override void Send(Message message)
         {
             if (!IsConnected())
                 return;
